8573066feda1cbfdb3c493ba279e4bfe6ced2a15
Stephan Florquin <stephan.florquin@gmail.com>
2016-07-22 15:18:02 +0200

    Expose functions

---
+++ packages/agent-js/src/index.js
@@ -0,0 +1,9 @@
+import agent from './agent';
+import agentHttpServer from './agentHttpServer';
+import storeHttpClient from './storeHttpClient';
+
+module.exports = {
+  agent,
+  agentHttpServer,
+  storeHttpClient
+};
