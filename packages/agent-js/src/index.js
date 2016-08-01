09d31961d9367b1754519cc6da3f5a489651cf5e
Stephan Florquin <stephan.florquin@gmail.com>
2016-08-01 15:51:05 +0200

    Expore memory store

--- packages/agent-js/src/index.js
+++ packages/agent-js/src/index.js
@@ -1,9 +1,11 @@
 import create from './create';
 import httpServer from './httpServer';
+import memoryStore from './memoryStore';
 import storeHttpClient from './storeHttpClient';
 
 module.exports = {
   create,
   httpServer,
+  memoryStore,
   storeHttpClient
 };
