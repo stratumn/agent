8d8e71a9a7244ad3865deeabbeaf943c9aed33fb
adrien <adrien.montfort@gmail.com>
2016-08-03 11:45:22 +0200

    angular-mapexplorer: Restore angular-drop and md-color-picker deps

--- packages/angular-mapexplorer/src/index.js
+++ packages/angular-mapexplorer/src/index.js
@@ -4,13 +4,12 @@ import stMapValidator from './stMapValidator.directive';
 import stPromiseLoader from './stPromiseLoader.directive';
 import stTagColorPicker from './stTagColorPicker.directive';
 import AceConfigurationService from './AceConfiguration.service';
-// import 'md-color-picker';
-// import 'angular-drop';
+import mdColorPicker from 'md-color-picker';
+import uiDrop from 'angular-drop/AngularDrop';
 import configureTemplates from './templates';
 
-
-angular.module('stratumn.angular-mapexplorer', []
-  // ['mdColorPicker', 'ui.drop']
+angular.module('stratumn.angular-mapexplorer',
+  [mdColorPicker, uiDrop]
 )
   .directive('stMapExplorer', stMapExplorer)
   .directive('stMerklePathTree', stMerklePathTree)
