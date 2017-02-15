817914282e76eb799fda0218250cbcfad8b72581
Stephan Florquin <stephan.florquin@gmail.com>
2017-02-15 17:39:35 +0100

    angular2-mapexplorer: change license to Mozilla Public License 2.0

--- packages/angular2-mapexplorer/src/st-map-explorer/st-map-explorer.component.ts
+++ packages/angular2-mapexplorer/src/st-map-explorer/st-map-explorer.component.ts
@@ -1,3 +1,11 @@
+/*
+    Copyright (C) 2017  Stratumn SAS
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 import { Component, Input, OnInit, OnChanges, AfterViewInit, ViewChild } from '@angular/core';
 import { ChainTreeBuilderService } from '../chain-tree-builder.service';
 import { ChainTreeBuilder } from 'mapexplorer-core';
