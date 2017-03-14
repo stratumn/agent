1d9dc20bbb3f80086534c90901b70beb6ae2ee0d
Adrien Montfort <adrien.montfort@gmail.com>
2017-03-14 17:53:39 +0100

    Fix setimmediate import

--- packages/agent-client-js/src/promiseWhile.js
+++ packages/agent-client-js/src/promiseWhile.js
@@ -6,7 +6,7 @@
     file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
-import 'setImmediate';
+import 'setimmediate';
 
 /**
  * Calls a function that returns a Promise until a condition is reached
