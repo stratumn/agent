31bb7a2b58a669dbc6f6976e0026672255aaad4b
Jeremie Dayan <jeremie@stratumn.com>
2017-09-26 19:03:27 +0200

    agent object

--- packages/agent-client-js/test/utils/testSetUp.js
+++ packages/agent-client-js/test/utils/testSetUp.js
@@ -5,11 +5,11 @@ const agentUrl = `http://localhost:${port}`;
 let agent = dummyAgent(port);
 let closeServer;
 
-beforeEach(() => {agent = dummyAgent(port);});
+// beforeEach(() => {agent = dummyAgent(port);});
 beforeEach(() => _agentHttpServer(agent, port).then(c => { closeServer = c; }));
 afterEach(() => {closeServer();});
 
 export default () => ({
-  // 'agent object': [agent]
-  'agent url': [agentUrl]
+  'agent object': [agent]
+  // 'agent url': [agentUrl]
 });
