036f4b23e028876018d03869e9e32295e1a8af94
Jeremie Dayan <jeremie@stratumn.com>
2017-09-26 18:03:10 +0200

    agent url only

--- packages/agent-client-js/test/utils/testSetUp.js
+++ packages/agent-client-js/test/utils/testSetUp.js
@@ -5,12 +5,11 @@ const agentUrl = `http://localhost:${port}`;
 let agent = dummyAgent(port);
 let closeServer;
 
-beforeEach(() => {agent = dummyAgent(port); console.log('before')});
-afterEach(() => agent.findSegments('first_process').then(res => {console.log(JSON.stringify(res, null, 2));}));
-// beforeEach(() => _agentHttpServer(agent, port).then(c => { closeServer = c; }));
-// afterEach(() => {closeServer();});
+beforeEach(() => {agent = dummyAgent(port);});
+beforeEach(() => _agentHttpServer(agent, port).then(c => { closeServer = c; }));
+afterEach(() => {closeServer();});
 
 export default () => ({
-  'agent object': [agent]
-  // 'agent url': [agentUrl]
+  // 'agent object': [agent]
+  'agent url': [agentUrl]
 });
