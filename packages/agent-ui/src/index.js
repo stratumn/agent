abd1019f116de150629404979164d0ee8e02f540
t-bast <bastien@stratumn.com>
2017-10-09 17:05:20 +0200

    get to a minimal working app

--- packages/agent-ui/src/index.js
+++ packages/agent-ui/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import { Router, Route, browserHistory } from 'react-router'
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
-import App from './components/App'
 
 import reducers from './reducers'
+import App from './components/App'
 
-// Add the reducer to your store on the `routing` key
 const store = createStore(
   combineReducers({
     ...reducers,
@@ -21,7 +21,6 @@ const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
   <Provider store={store}>
-    { /* Tell the Router to use our enhanced history */ }
     <Router history={history}>
       <Route path="/" component={App} />
     </Router>
