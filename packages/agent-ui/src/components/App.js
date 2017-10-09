fa34c12bf64edc2c2de834bf9f2d57acca1324ef
t-bast <bastien@stratumn.com>
2017-10-09 18:56:32 +0200

    agent-ui: remove unnecessary css and minimal material-ui app

--- packages/agent-ui/src/components/App.js
+++ packages/agent-ui/src/components/App.js
@@ -1,19 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { AppBar, Drawer, MenuItem } from 'material-ui'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/components/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <MuiThemeProvider>
+        <AppBar title="Agent info" />
+      </MuiThemeProvider>
+    )
   }
 }
 
-export default App;
+export default App
