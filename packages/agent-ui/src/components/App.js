1f98e4b32f947c18eb6e876f6a842caf46531ef2
Jeremie Dayan <jeremie@stratumn.com>
2017-10-10 18:14:17 +0200

    cleanup

--- packages/agent-ui/src/components/App.js
+++ packages/agent-ui/src/components/App.js
@@ -2,30 +2,17 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
-import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
 
+import TopBar from './topBar';
+import LeftDrawer from './leftDrawer';
 
-const LeftDrawer = ({processes}) => {
-  // const menuItems = [];
-  const menuItems = Object.keys(processes).map(p => (
-    <MenuItem key={p}>{p}</MenuItem>
-  ));
-  console.log('leftdrawer', processes);
-  return (
-    <Drawer>
-      {menuItems}
-    </Drawer>
-  );
-};
 class App extends Component {
   render() {
     console.log('App render', this.props);
     return (
       <MuiThemeProvider>
         <div>
-          <AppBar title="Agent info" />
+          <TopBar/>
           <LeftDrawer processes={this.props.processes} />
         </div>
       </MuiThemeProvider>
--- packages/agent-ui/src/index.js
+++ packages/agent-ui/src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 
-import App from './components/App';
+import routes from './routes';
 import configureStore from './store/configure';
 import { getAgentInfo } from './actions/getAgentInfo'
 
@@ -17,9 +17,7 @@ const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
-      <Route path="/" component={App} />
-    </Router>
+    <Router history={history} routes={routes} />
   </Provider>,
   document.getElementById('root')
 )
