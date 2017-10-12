23cccab0bd12f12c5756ca18080807415839ff3c
Jeremie Dayan <jeremie@stratumn.com>
2017-10-12 17:31:15 +0200

    add maps and segments submenu

--- packages/agent-ui/src/components/leftDrawer.js
+++ packages/agent-ui/src/components/leftDrawer.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 
-const LeftDrawer = ({ processes }) => {
+const ActiveMenuItem = ({ match, item }) => {
+  const toLink = match.url === '/' ? `/${item}` : `${match.url}/${item}`;
+  return (
+    <NavLink to={`${toLink}`} activeStyle={{ fontWeight: 'bold' }}>
+      <MenuItem>{item}</MenuItem>
+    </NavLink>
+  );
+};
+
+const ActiveSubMenuItem = ({ match }) => {
+  console.log('ActiveSubMenuItem', match);
+  return (
+    <div>
+      <ActiveMenuItem item="maps" match={match} />
+      <ActiveMenuItem item="segments" match={match} />
+    </div>
+  );
+};
+
+const LeftDrawer = ({ processes, match }) => {
+  console.log('leftdrawer', match);
   const menuItems = processes.map(p => (
-    <Link key={p} to={`/${p}`}>
-      <MenuItem>{p}</MenuItem>
-    </Link>
+    <div key={p}>
+      <ActiveMenuItem item={p} match={match} />
+      <Route path={`/${p}`} component={ActiveSubMenuItem} {...this.props} />
+    </div>
   ));
   console.log('leftdrawer', processes);
   return <Drawer>{menuItems}</Drawer>;
