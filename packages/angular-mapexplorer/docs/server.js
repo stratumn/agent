5b75661f923a07a2401fee5d8650fd8de89bcf3e
Adrien Montfort <adrien.montfort@gmail.com>
2017-10-12 10:36:13 +0200

    angular-mapexplorer: adapt to new indigo version

--- packages/angular-mapexplorer/docs/server.js
+++ packages/angular-mapexplorer/docs/server.js
@@ -1,34 +1,33 @@
-'use strict';
+const fs = require('fs'); // for image upload file handling
 
-var fs =require('fs');		//for image upload file handling
+const express = require('express');
 
-var express = require('express');
-var app = express();
+const app = express();
 
-var port =3000;
-var host ='localhost';
-var serverPath ='/';
-var staticPath ='/';
+const port = 3300;
+const host = 'localhost';
+const serverPath = '/';
+const staticPath = '/';
 
-var staticFilePath = __dirname + serverPath;
+let staticFilePath = __dirname + serverPath;
 // remove trailing slash if present
-if(staticFilePath.substr(-1) === '/'){
-	staticFilePath = staticFilePath.substr(0, staticFilePath.length - 1);
+if (staticFilePath.substr(-1) === '/') {
+  staticFilePath = staticFilePath.substr(0, staticFilePath.length - 1);
 }
 
-app.configure(function(){
-	// compress static content
-	app.use(express.compress());
-	app.use(serverPath, express.static(staticFilePath));		//serve static files
-	
-	app.use(express.bodyParser());		//for post content / files - not sure if this is actually necessary?
+app.configure(() => {
+  // compress static content
+  app.use(express.compress());
+  app.use(serverPath, express.static(staticFilePath)); // serve static files
+
+  app.use(express.bodyParser()); // for post content / files - not sure if this is actually necessary?
 });
 
-//catch all route to serve index.html (main frontend app)
-app.get('*', function(req, res){
-	res.sendfile(staticFilePath + staticPath+ 'index.html');
+// catch all route to serve index.html (main frontend app)
+app.get('*', (req, res) => {
+  res.sendfile(`${staticFilePath + staticPath}index.html`);
 });
 
 app.listen(port);
 
-console.log('Server running at http://'+host+':'+port.toString()+'/');
+console.log(`Server running at http://${host}:${port.toString()}/`);
