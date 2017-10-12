d6f965859bd523db35d0b9d9ba3dae24698c9e65
Adrien Montfort <adrien.montfort@gmail.com>
2017-10-12 11:31:01 +0200

    mapexplorer-core: add code coverage

--- packages/mapexplorer-core/karma.conf.js
+++ packages/mapexplorer-core/karma.conf.js
@@ -16,7 +16,8 @@ module.exports = function(config) {
 
     preprocessors: {
       'test/integration/init.js': ['rollup'],
-      'test/integration/*.test.js': ['rollup']
+      'test/integration/*.test.js': ['rollup'],
+      'src/**/*.js': ['coverage']
     },
 
     rollupPreprocessor: {
@@ -34,28 +35,26 @@ module.exports = function(config) {
     // list of files to exclude
     exclude: [],
 
-
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
+    coverageReporter: {
+      reporters: [{ type: 'lcov' }]
+    },
 
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // Start these browsers, currently available:
     // - Chrome
@@ -67,10 +66,8 @@ module.exports = function(config) {
     // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
     browsers: ['PhantomJS', 'Chrome'],
 
-
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 60000,
-
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
