48dd37aabb7eb506c8aa6020320e25511e88f36c
Bastien Teinturier <31281497+t-bast@users.noreply.github.com>
2017-10-12 10:59:52 +0200

    agent-ui: add the AgentInfoPage component (#38)

---
+++ packages/agent-ui/src/components/agentInfoPage.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class AgentInfoPage extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          An agent executes the logic of your processes. A process is defined by
+          a set of actions that may be used in the workflow.<br />
+          An instance of a process is called a map. It contains the different
+          steps of the process, called segments.
+        </p>
+        <hr />
+        <h2>Endpoint</h2>
+        <p>
+          <a
+            href={this.props.agentUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {this.props.agentUrl}
+          </a>
+        </p>
+        <p>
+          <small>
+            You can use this URL to connect to your agent, using the{' '}
+            <a
+              href="https://github.com/stratumn/indigo-js/tree/master/packages/agent-client-js"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Stratumn Javascript Agent Client
+            </a>{' '}
+            for instance.
+          </small>
+        </p>
+        <hr />
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  let agentUrl = '';
+  if (state.agentInfo) {
+    agentUrl = state.agentInfo.url;
+  }
+  return { agentUrl };
+}
+
+AgentInfoPage.propTypes = {
+  agentUrl: PropTypes.string.isRequired
+};
+
+export default connect(mapStateToProps)(AgentInfoPage);
--- packages/agent-ui/src/components/index.js
+++ packages/agent-ui/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as App } from './app';
 export { default as LeftDrawer } from './leftDrawer';
 export { default as TopBar } from './topBar';
+export { default as AgentInfoPage } from './agentInfoPage';
