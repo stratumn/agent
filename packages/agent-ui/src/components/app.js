924077967142f6a363fcdc95b63ce204b10f1a9b
Jeremie Dayan <jeremie@stratumn.com>
2017-10-13 11:15:29 +0200

    do not use multi theme provider

--- packages/agent-ui/src/components/app.js
+++ packages/agent-ui/src/components/app.js
@@ -3,19 +3,15 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter, Route } from 'react-router-dom';
 
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
 import { TopBar, LeftDrawer, AgentInfoPage, ProcessInfoPage } from './';
 
 export const App = ({ processes }) => (
-  <MuiThemeProvider>
-    <div>
-      <TopBar />
-      <LeftDrawer processes={processes} />
-      <Route exact path="/" component={AgentInfoPage} />
-      <Route exact path="/:process" component={ProcessInfoPage} />
-    </div>
-  </MuiThemeProvider>
+  <div>
+    <TopBar />
+    <LeftDrawer processes={processes} />
+    <Route exact path="/" component={AgentInfoPage} />
+    <Route exact path="/:process" component={ProcessInfoPage} />
+  </div>
 );
 
 function mapStateToProps(state) {
