6f2838618a3d9d98bccb9bc053e22a6bf4b69de3
Jeremie Dayan <jeremie@stratumn.com>
2017-10-13 10:56:05 +0200

    fix rebase

--- packages/agent-ui/src/components/topBar.js
+++ packages/agent-ui/src/components/topBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+// import { Route } from 'react-router-dom';
 
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
--- packages/agent-ui/src/routes.js
+++ packages/agent-ui/src/routes.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import { Route } from 'react-router-dom';
 
 import { App, AgentInfoPage, ProcessInfoPage } from './components';
 
 export default (
-  <Route path="/" component={App}>
-    <IndexRoute component={AgentInfoPage} />
-    <Route path=":process" component={ProcessInfoPage} />
-  </Route>
+  <div>
+    <Route path="/" component={App} />
+    <Route exact path="/" component={AgentInfoPage} />
+    <Route exact path="/:process" component={ProcessInfoPage} />
+  </div>
 );
