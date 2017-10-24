4263d8cf9955996b1ce5edd038d481db0eaeda95
Bastien Teinturier <31281497+t-bast@users.noreply.github.com>
2017-10-24 10:31:03 +0200

    agent-ui: add redux devtools (#71)

--- packages/agent-ui/src/store/configure.js
+++ packages/agent-ui/src/store/configure.js
@@ -1,7 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers';
 
-const configureStore = () => createStore(rootReducer, applyMiddleware(thunk));
+/* It's ok (even encouraged) to include the Redux dev tools in production:
+ * https://medium.com/@zalmoxis/using-redux-devtools-in-production-4c5b56c5600f
+ * If we need to improve perf, we can use redux-devtools-extension/logOnlyInProduction
+ */
+const configureStore = () =>
+  createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default configureStore;
