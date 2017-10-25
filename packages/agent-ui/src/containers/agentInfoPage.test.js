a880d79cc589264003d82cb16576552617191e85
t-bast <bastien@stratumn.com>
2017-10-25 10:17:07 +0200

    agent-info-page: define scenarios in unit tests

---
+++ packages/agent-ui/src/containers/agentInfoPage.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { mount } from 'enzyme';
+import { expect } from 'chai';
+
+import { AgentInfoPage } from './agentInfoPage';
+
+describe('<AgentInfoPage />', () => {
+  it('displays the agent name and url', () => {});
+  it('provides a button to refresh agent', () => {});
+  it('displays a custom message when agent was not loaded', () => {});
+  it('provides a link to the add agent page when agent was not loaded', () => {});
+});
