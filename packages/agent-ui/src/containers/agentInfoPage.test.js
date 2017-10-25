ad46d84c72a4e1dfd1d79faf2314d8e0879e54f5
t-bast <bastien@stratumn.com>
2017-10-25 10:51:33 +0200

    agent-info-page: test name and url

--- packages/agent-ui/src/containers/agentInfoPage.test.js
+++ packages/agent-ui/src/containers/agentInfoPage.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 
 import { mount } from 'enzyme';
 import { expect } from 'chai';
@@ -6,8 +7,21 @@ import { expect } from 'chai';
 import { AgentInfoPage } from './agentInfoPage';
 
 describe('<AgentInfoPage />', () => {
-  it('displays the agent name and url', () => {});
+  it('displays the agent name and url', () => {
+    const agentName = 'test';
+    const agentUrl = 'http://localhost:3000';
+    const agentInfoPage = mount(
+      <MemoryRouter>
+        <AgentInfoPage name={agentName} url={agentUrl} />
+      </MemoryRouter>
+    );
+
+    const agentInfo = agentInfoPage.find('p').map(p => p.text());
+    expect(agentInfo.includes(agentName)).to.equal(true);
+    expect(agentInfo.includes(agentUrl)).to.equal(true);
+  });
+
   it('provides a button to refresh agent', () => {});
   it('displays a custom message when agent was not loaded', () => {});
-  it('provides a link to the add agent page when agent was not loaded', () => {});
+  it('provides a link to the add agent page', () => {});
 });
