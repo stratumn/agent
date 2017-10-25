a67f11c6b1e17463122cc4425489483c2521ff8e
t-bast <bastien@stratumn.com>
2017-10-25 11:00:50 +0200

    agent-info-page: add a refresh button

--- packages/agent-ui/src/containers/agentInfoPage.js
+++ packages/agent-ui/src/containers/agentInfoPage.js
@@ -3,17 +3,28 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter, NavLink } from 'react-router-dom';
 
-export const AgentInfoPage = ({ name, url }) => (
+export const AgentInfoPage = ({ name, url, fetchAgent }) => (
   <div>
     <p>{name}</p>
     <p>{url}</p>
+    <button
+      onClick={e => {
+        e.preventDefault();
+        if (url) {
+          fetchAgent(name, url);
+        }
+      }}
+    >
+      Refresh
+    </button>
     <NavLink to="/">Add a new agent</NavLink>
   </div>
 );
 
 AgentInfoPage.propTypes = {
   name: PropTypes.string.isRequired,
-  url: PropTypes.string.isRequired
+  url: PropTypes.string.isRequired,
+  fetchAgent: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
--- packages/agent-ui/src/containers/agentInfoPage.test.js
+++ packages/agent-ui/src/containers/agentInfoPage.test.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 
 import { mount } from 'enzyme';
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
 
 import { AgentInfoPage } from './agentInfoPage';
+
+chai.use(sinonChai);
 
 describe('<AgentInfoPage />', () => {
   it('displays the agent name and url', () => {
@@ -21,7 +25,29 @@ describe('<AgentInfoPage />', () => {
     expect(agentInfo.includes(agentUrl)).to.equal(true);
   });
 
-  it('provides a button to refresh agent', () => {});
+  it('provides a button to refresh agent', () => {
+    const fetchAgentSpy = sinon.spy();
+    const agentInfoPage = mount(
+      <MemoryRouter>
+        <AgentInfoPage
+          name="dummy"
+          url="http://my.awesome.agent"
+          fetchAgent={fetchAgentSpy}
+        />
+      </MemoryRouter>
+    );
+
+    const refreshButton = agentInfoPage.find('button');
+    expect(refreshButton.length).to.equal(1);
+
+    refreshButton.simulate('click');
+    expect(fetchAgentSpy.callCount).to.equal(1);
+    const fetchAgentArgs = fetchAgentSpy.getCall(0).args;
+    expect(fetchAgentArgs.length).to.equal(2);
+    expect(fetchAgentArgs[0]).to.equal('dummy');
+    expect(fetchAgentArgs[1]).to.equal('http://my.awesome.agent');
+  });
+
   it('displays a custom message when agent was not loaded', () => {});
   it('provides a link to the add agent page', () => {});
 });
