0b01cae1c0ec61a5e939e142686935d19330dc5b
t-bast <bastien@stratumn.com>
2017-10-26 18:30:06 +0200

    agent-ui: top bar provides links to sub-parts of the path

--- packages/agent-ui/src/containers/topBar.js
+++ packages/agent-ui/src/containers/topBar.js
@@ -1,29 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { withRouter, NavLink } from 'react-router-dom';
 
-export const TopBar = ({ path }) => (
-  <div
-    style={{
-      position: 'absolute',
-      width: 'calc(100% - 240px)',
-      height: '56px',
-      marginLeft: '240px',
-      borderStyle: 'solid'
-    }}
-  >
-    {path}
-  </div>
-);
+const renderTopBarLinks = path => {
+  const parts = path.split('/').filter(p => p);
+  let currentLink = '';
+  return parts.map(p => {
+    currentLink += `/${p}`;
+    return (
+      <span key={p}>
+        {' > '}
+        <NavLink key={p} to={currentLink}>
+          {p}
+        </NavLink>
+      </span>
+    );
+  });
+};
+
+export const TopBar = ({ path }) => {
+  const style = {
+    position: 'absolute',
+    width: 'calc(100% - 240px)',
+    height: '56px',
+    marginLeft: '240px',
+    borderStyle: 'solid'
+  };
+
+  if (!path || path === '/') {
+    return <div style={style}>Welcome to the Indigo Framework UI</div>;
+  }
+
+  return <div style={style}>{renderTopBarLinks(path)}</div>;
+};
 
 TopBar.propTypes = {
   path: PropTypes.string.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
-  console.log('TopBar state', state);
-  console.log('topBar ownProps', ownProps);
   return {
     path: ownProps.location.pathname
   };
