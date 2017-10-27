97eaf1c1d2a6bc9de3d1b1324af3682e832d1ec1
t-bast <bastien@stratumn.com>
2017-10-27 11:50:30 +0200

    agent-ui: use map explorer in the map page

--- packages/agent-ui/src/containers/mapPage.js
+++ packages/agent-ui/src/containers/mapPage.js
@@ -1,13 +1,57 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { MapExplorer } from 'react-mapexplorer';
 
-export const MapPage = () => <div>map info</div>;
+export const MapPage = ({ agent, process, mapId }) => {
+  if (!agent.url) {
+    return (
+      <div className="error">
+        Invalid agent. Make sure the url is correct and the agent is loaded.
+      </div>
+    );
+  }
 
-function mapStateToProps(state, ownProps) {
-  console.log('MapPage state', state);
-  console.log('MapPage ownProps', ownProps);
-  return {};
+  return (
+    <div>
+      <MapExplorer agentUrl={agent.url} process={process.name} mapId={mapId} />
+    </div>
+  );
+};
+
+MapPage.propTypes = {
+  agent: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    url: PropTypes.string
+  }).isRequired,
+  process: PropTypes.shape({
+    name: PropTypes.string.isRequired
+  }).isRequired,
+  mapId: PropTypes.string.isRequired
+};
+
+export function mapStateToProps(state, ownProps) {
+  const {
+    match: { params: { agent: agentName, process: processName, id: mapId } }
+  } = ownProps;
+  const agent = {
+    name: agentName
+  };
+  const process = {
+    name: processName
+  };
+
+  const agentFromState = state.agents[agentName];
+  if (agentFromState && agentFromState.url) {
+    agent.url = agentFromState.url;
+  }
+
+  return {
+    agent,
+    process,
+    mapId
+  };
 }
 
 export default withRouter(connect(mapStateToProps)(MapPage));
---
+++ packages/agent-ui/src/containers/mapPage.test.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { MapExplorer } from 'react-mapexplorer';
+
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+
+import { statusTypes } from '../reducers';
+
+import {
+  TestAgentBuilder,
+  TestProcessBuilder,
+  TestStateBuilder
+} from '../test/builders/state';
+
+import { MapPage, mapStateToProps } from './mapPage';
+
+describe('<MapPage />', () => {
+  const validProps = {
+    agent: {
+      name: 'myAgent',
+      url: 'http://localhost:42'
+    },
+    process: {
+      name: 'proc42'
+    },
+    mapId: '42'
+  };
+
+  it('should render a MapExplorer component', () => {
+    const mapPage = shallow(<MapPage {...validProps} />);
+    expect(mapPage.find(MapExplorer)).to.have.length(1);
+  });
+
+  it('should display an error if agent url is missing', () => {
+    const mapPage = shallow(<MapPage agent={{ name: 'missing-url' }} />);
+    expect(mapPage.find(MapExplorer)).to.have.length(0);
+    expect(mapPage.find('.error')).to.have.length(1);
+  });
+
+  it('should extract agent, process and map information', () => {
+    const process = new TestProcessBuilder('p1').build();
+    const agent = new TestAgentBuilder()
+      .withUrl('http://localhost:4242')
+      .withProcess(process)
+      .build();
+    const state = new TestStateBuilder().withAgent('agent', agent);
+    const routeProps = {
+      match: { params: { agent: 'agent', process: 'p1', id: '42' } }
+    };
+
+    const props = mapStateToProps(state, routeProps);
+    expect(props).to.deep.equal({
+      agent: {
+        name: 'agent',
+        url: 'http://localhost:4242'
+      },
+      process: {
+        name: 'p1'
+      },
+      mapId: '42'
+    });
+  });
+
+  it('should not fail props if agent or process is missing', () => {
+    const agent = new TestAgentBuilder().withStatus(statusTypes.FAILED).build();
+    const state = new TestStateBuilder().withAgent('a', agent).build();
+    const routeProps = {
+      match: { params: { agent: 'a', process: 'cannotFindMe', id: '42' } }
+    };
+
+    const props = mapStateToProps(state, routeProps);
+    expect(props).to.deep.equal({
+      agent: {
+        name: 'a'
+      },
+      process: {
+        name: 'cannotFindMe'
+      },
+      mapId: '42'
+    });
+  });
+});
