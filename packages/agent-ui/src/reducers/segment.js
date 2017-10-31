7cddd530edaf4d0ff3fb5a3624d8fd95249f1ea7
t-bast <bastien@stratumn.com>
2017-10-31 15:44:31 +0100

    agent-ui: define segment reducer

---
+++ packages/agent-ui/src/reducers/segment.js
@@ -0,0 +1,28 @@
+import { actionTypes } from '../actions';
+import { statusTypes } from './';
+
+export default function(state = {}, action) {
+  switch (action.type) {
+    case actionTypes.SEGMENT_REQUEST:
+      return {
+        status: statusTypes.LOADING,
+        agent: action.agent,
+        process: action.process,
+        linkHash: action.linkHash
+      };
+    case actionTypes.SEGMENT_FAILURE:
+      return {
+        ...state,
+        status: statusTypes.FAILED,
+        error: action.error
+      };
+    case actionTypes.SEGMENT_SUCCESS:
+      return {
+        ...state,
+        status: statusTypes.LOADED,
+        details: action.segment
+      };
+    default:
+      return state;
+  }
+}
---
+++ packages/agent-ui/src/reducers/segment.test.js
@@ -0,0 +1,57 @@
+import { expect } from 'chai';
+
+import segment from './segment';
+
+import { actionTypes } from '../actions';
+import { statusTypes } from './';
+
+describe('segment reducer', () => {
+  const requestSegmentAction = {
+    type: actionTypes.SEGMENT_REQUEST,
+    agent: 'a',
+    process: 'p',
+    linkHash: 'linkHash'
+  };
+
+  it('sets agent and process names, segment linkHash on request', () => {
+    const requestState = segment({}, requestSegmentAction);
+
+    expect(requestState).to.deep.equal({
+      status: statusTypes.LOADING,
+      agent: 'a',
+      process: 'p',
+      linkHash: 'linkHash'
+    });
+  });
+
+  it('adds error information on failure', () => {
+    const failedState = segment(segment({}, requestSegmentAction), {
+      type: actionTypes.SEGMENT_FAILURE,
+      error: 'failed'
+    });
+
+    expect(failedState).to.deep.equal({
+      status: statusTypes.FAILED,
+      agent: 'a',
+      process: 'p',
+      linkHash: 'linkHash',
+      error: 'failed'
+    });
+  });
+
+  it('adds segment details on success', () => {
+    const validSegment = { name: 'I am a valid segment' };
+    const successState = segment(segment({}, requestSegmentAction), {
+      type: actionTypes.SEGMENT_SUCCESS,
+      segment: validSegment
+    });
+
+    expect(successState).to.deep.equal({
+      status: statusTypes.LOADED,
+      agent: 'a',
+      process: 'p',
+      linkHash: 'linkHash',
+      details: validSegment
+    });
+  });
+});
