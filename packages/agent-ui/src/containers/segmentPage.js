7924ef85822e03d2869fb36f1ffdd4d7ffeb5295
t-bast <bastien@stratumn.com>
2017-10-31 15:44:46 +0100

    agent-ui: segment page

--- packages/agent-ui/src/containers/segmentPage.js
+++ packages/agent-ui/src/containers/segmentPage.js
@@ -1,13 +1,59 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
-export const SegmentPage = () => <div>segment info</div>;
+import { statusTypes } from '../reducers';
 
-function mapStateToProps(state, ownProps) {
-  console.log('SegmentPage state', state);
-  console.log('SegmentPage ownProps', ownProps);
-  return {};
+export const SegmentPage = ({ status, error, segment }) => {
+  if (status === statusTypes.LOADING) {
+    return <div>loading...</div>;
+  }
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+
+  if (!segment) {
+    return <div className="error">Could not find segment</div>;
+  }
+
+  return <div>{JSON.stringify(segment)}</div>;
+};
+
+SegmentPage.defaultProps = {
+  error: '',
+  segment: undefined
+};
+SegmentPage.propTypes = {
+  status: PropTypes.string.isRequired,
+  error: PropTypes.string,
+  segment: PropTypes.shape({
+    meta: PropTypes.shape({
+      linkHash: PropTypes.string.isRequired
+    }).isRequired
+  })
+};
+
+export function mapStateToProps(state) {
+  if (!state.segment) {
+    return {
+      error: 'No matching segment found'
+    };
+  }
+
+  const { status, error, details } = state.segment;
+  if (error) {
+    return {
+      status,
+      error
+    };
+  }
+
+  return {
+    status,
+    segment: details
+  };
 }
 
 export default withRouter(connect(mapStateToProps)(SegmentPage));
