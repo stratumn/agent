c3b411194335e2d900ee1af183caaac0151579df
Jeremie Dayan <jeremie@stratumn.com>
2017-10-31 17:07:09 +0100

    change import location or storage

--- packages/agent-ui/src/store/configure.js
+++ packages/agent-ui/src/store/configure.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistReducer, persistStore } from 'redux-persist';
-import storage from 'redux-persist/es/storage';
+import storage from 'redux-persist/lib/storage';
 import rootReducer from '../reducers';
 
 /* It's ok (even encouraged) to include the Redux dev tools in production:
