05253f0e0b043e64af5da973bfbf9dd2309f1d20
Jeremie Dayan <jeremie@stratumn.com>
2017-10-31 14:35:07 +0100

    fixoffending import Root

--- packages/agent-ui/src/containers/index.js
+++ packages/agent-ui/src/containers/index.js
@@ -3,7 +3,7 @@
  * They are tightly coupled to the app ("smart components").
  * See http://redux.js.org/docs/basics/UsageWithReact.html
  */
-// export { default as Root } from './root';
+export { default as Root } from './root';
 export { default as App } from './app';
 export { default as TopBar } from './topBar';
 export { default as LeftNavigation } from './leftNavigation';
--- packages/agent-ui/src/containers/root.js
+++ packages/agent-ui/src/containers/root.js
@@ -1,4 +1,4 @@
-import { PersistGate } from 'redux-persist/es/integration/react';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
