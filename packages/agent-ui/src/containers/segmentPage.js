76e7cf1b95a6dadb8c834a06780c84ef2fc51beb
t-bast <bastien@stratumn.com>
2017-11-02 12:07:04 +0100

    agent-ui: segment page fetches segment when needed

--- packages/agent-ui/src/containers/segmentPage.js
+++ packages/agent-ui/src/containers/segmentPage.js
@@ -1,59 +1,94 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
+import { getSegment } from '../actions';
 import { statusTypes } from '../reducers';
 
-export const SegmentPage = ({ status, error, segment }) => {
-  if (status === statusTypes.LOADING) {
-    return <div>loading...</div>;
+export class SegmentPage extends Component {
+  componentDidMount() {
+    const { agent, process, linkHash, refresh, getSegmentAction } = this.props;
+    if (refresh) {
+      getSegmentAction(agent, process, linkHash);
+    }
   }
 
-  if (error) {
-    return <div className="error">{error}</div>;
+  componentWillReceiveProps(nextProps) {
+    const { agent, process, linkHash, refresh, getSegmentAction } = nextProps;
+    if (refresh) {
+      getSegmentAction(agent, process, linkHash);
+    }
   }
 
-  if (!segment) {
-    return <div className="error">Could not find segment</div>;
-  }
+  render() {
+    const { status, error, segment } = this.props;
+    if (status === statusTypes.LOADING) {
+      return <div>loading...</div>;
+    }
 
-  return <div>{JSON.stringify(segment)}</div>;
-};
+    if (error) {
+      return <div className="error">{error}</div>;
+    }
+
+    if (!segment) {
+      return <div className="error">Could not find segment</div>;
+    }
+
+    return <div>{JSON.stringify(segment)}</div>;
+  }
+}
 
 SegmentPage.defaultProps = {
+  status: '',
   error: '',
   segment: undefined
 };
+
 SegmentPage.propTypes = {
-  status: PropTypes.string.isRequired,
+  getSegmentAction: PropTypes.func.isRequired,
+  agent: PropTypes.string.isRequired,
+  process: PropTypes.string.isRequired,
+  linkHash: PropTypes.string.isRequired,
+  refresh: PropTypes.bool.isRequired,
+  status: PropTypes.string,
   error: PropTypes.string,
   segment: PropTypes.shape({
     meta: PropTypes.shape({
       linkHash: PropTypes.string.isRequired
-    }).isRequired
+    })
   })
 };
 
-export function mapStateToProps(state) {
+export function mapStateToProps(state, ownProps) {
+  const { match: { params: { agent, process, id: linkHash } } } = ownProps;
+  const props = {
+    agent,
+    process,
+    linkHash,
+    refresh: true
+  };
+
   if (!state.segment) {
-    return {
-      error: 'No matching segment found'
-    };
+    return props;
   }
 
   const { status, error, details } = state.segment;
-  if (error) {
-    return {
-      status,
-      error
-    };
+  props.status = status;
+
+  if (status === statusTypes.LOADING) {
+    props.refresh = false;
+  } else if (status === statusTypes.FAILED) {
+    props.refresh = false;
+    props.error = error;
+  } else {
+    props.refresh = !details || details.meta.linkHash !== linkHash;
+    props.segment = details;
   }
 
-  return {
-    status,
-    segment: details
-  };
+  return props;
 }
 
-export default withRouter(connect(mapStateToProps)(SegmentPage));
+export default withRouter(
+  connect(mapStateToProps, { getSegmentAction: getSegment })(SegmentPage)
+);
--- packages/agent-ui/src/containers/segmentPage.test.js
+++ packages/agent-ui/src/containers/segmentPage.test.js
@@ -1,43 +1,90 @@
 import React from 'react';
 
-import { shallow } from 'enzyme';
-import { expect } from 'chai';
+import { mount, shallow } from 'enzyme';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
 
 import { statusTypes } from '../reducers';
 
 import { SegmentPage, mapStateToProps } from './segmentPage';
 
+chai.use(sinonChai);
+
 describe('<SegmentPage />', () => {
-  const segment = { meta: { data: 'this is so meta' } };
+  const segment = { meta: { linkHash: 'this is so meta' } };
+  const routeWithLinkHash = { match: { params: { id: 'l' } } };
 
-  it('correctly extracts segment from state', () => {
-    const state = { segment: { status: statusTypes.LOADED, details: segment } };
-    const props = mapStateToProps(state);
-    expect(props).to.deep.equal({
-      status: statusTypes.LOADED,
-      segment: segment
-    });
+  it('refreshes segment if not initialized', () => {
+    const state = {};
+    const props = mapStateToProps(state, routeWithLinkHash);
+    expect(props.refresh).to.be.true;
   });
 
-  it('returns an error if no segment data in state', () => {
-    const state = { segment: null };
-    const props = mapStateToProps(state);
-    expect(props.error).to.exist;
+  it('refreshes segment if mismatch with url', () => {
+    const state = { segment: { details: segment } };
+    const props = mapStateToProps(state, routeWithLinkHash);
+    expect(props.refresh).to.be.true;
   });
+
+  it('does not refresh segment if already loading', () => {
+    const state = { segment: { status: statusTypes.LOADING } };
+    const props = mapStateToProps(state, routeWithLinkHash);
+    expect(props.refresh).to.be.false;
+    expect(props.status).to.equal(statusTypes.LOADING);
+  });
+
+  it('does not refresh segment if fetch failed', () => {
+    const state = { segment: { status: statusTypes.FAILED, error: 'err!!!' } };
+    const props = mapStateToProps(state, routeWithLinkHash);
+    expect(props.refresh).to.be.false;
+    expect(props.status).to.equal(statusTypes.FAILED);
+    expect(props.error).to.equal('err!!!');
+  });
+
+  const requiredProps = {
+    getSegmentAction: () => {},
+    agent: 'a',
+    process: 'p',
+    linkHash: 'l',
+    refresh: false
+  };
 
   it('displays a loading screen', () => {
-    const segmentPage = shallow(<SegmentPage status={statusTypes.LOADING} />);
+    const segmentPage = shallow(
+      <SegmentPage {...requiredProps} status={statusTypes.LOADING} />
+    );
     expect(segmentPage.find('div').contains('loading...')).to.equal(true);
   });
 
   it('displays an error message when segment is not found', () => {
-    const segmentPage = shallow(<SegmentPage />);
+    const segmentPage = shallow(<SegmentPage {...requiredProps} />);
     expect(segmentPage.find('.error')).to.have.length(1);
   });
 
   it('displays segment information', () => {
-    const segmentPage = shallow(<SegmentPage segment={segment} />);
+    const segmentPage = shallow(
+      <SegmentPage {...requiredProps} segment={segment} />
+    );
     expect(segmentPage.find('div').contains(JSON.stringify(segment))).to.be
       .true;
+  });
+
+  it('refreshes the segment when outdated', () => {
+    const segmentSpy = sinon.spy();
+    const props = {
+      getSegmentAction: segmentSpy,
+      agent: 'a',
+      process: 'p',
+      linkHash: 'l',
+      refresh: true
+    };
+
+    const segmentPage = mount(<SegmentPage {...props} />);
+    expect(segmentSpy.callCount).to.equal(1); // initial fetch
+
+    segmentPage.setProps({ ...props, linkHash: 'll' });
+    expect(segmentSpy.callCount).to.equal(2);
+    expect(segmentSpy.getCall(1).args).to.deep.equal(['a', 'p', 'll']);
   });
 });
