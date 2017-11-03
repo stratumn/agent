75db179011a822e96ce58dc070906708085ff785
t-bast <bastien@stratumn.com>
2017-11-03 16:42:54 +0100

    agent-ui: remove console logs

--- packages/agent-ui/src/containers/app.js
+++ packages/agent-ui/src/containers/app.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 import { TopBar, LeftNavigation, ContentPage, CreateMapDialog } from './';
@@ -13,10 +12,4 @@ export const App = () => (
   </div>
 );
 
-function mapStateToProps(state, ownProps) {
-  console.log('App state', state);
-  console.log('App ownProps', ownProps);
-  return {};
-}
-
-export default withRouter(connect(mapStateToProps)(App));
+export default withRouter(App);
