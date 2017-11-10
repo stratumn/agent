44529ce1c0532ad4b236f75719087f9cdd0a9da1
Jeremie Dayan <jeremie@stratumn.com>
2017-11-10 20:53:04 +0100

    agent-ui: left navigation formatting

--- packages/agent-ui/src/containers/leftNavigation.js
+++ packages/agent-ui/src/containers/leftNavigation.js
@@ -1,29 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { withRouter, NavLink } from 'react-router-dom';
+import { withRouter, NavLink, Route } from 'react-router-dom';
 import * as statusTypes from '../constants/status';
 
-const LeftLink = ({ to, item }) => (
-  <div>
-    <NavLink to={to}>{item}</NavLink>
-  </div>
-);
+const LeftLink = ({ to, item, margin }) => {
+  const navLink = () => (
+    <div>
+      <NavLink to={to} style={{ marginLeft: margin }}>
+        {item}
+      </NavLink>
+    </div>
+  );
+  const path = to
+    .split('/')
+    .slice(0, -1)
+    .join('/');
+  return <Route path={path} component={navLink} />;
+};
 
 LeftLink.propTypes = {
   to: PropTypes.string.isRequired,
-  item: PropTypes.string.isRequired
+  item: PropTypes.string.isRequired,
+  margin: PropTypes.string.isRequired
 };
 
 export const LeftNavigation = ({ agents }) => {
   const agentsList = agents.map(a => (
     <div key={a.name}>
-      <LeftLink to={`/${a.name}`} item={a.name} />
+      <LeftLink margin="0.5em" to={`/${a.name}`} item={a.name} />
       {a.processes.map(p => (
         <div key={p}>
-          <LeftLink to={`/${a.name}/${p}`} item={p} />
-          <LeftLink to={`/${a.name}/${p}/maps`} item="maps" />
-          <LeftLink to={`/${a.name}/${p}/segments`} item="segments" />
+          <LeftLink margin="1em" to={`/${a.name}/${p}`} item={p} />
+          <LeftLink margin="1.5em" to={`/${a.name}/${p}/maps`} item="maps" />
+          <LeftLink
+            margin="1.5em"
+            to={`/${a.name}/${p}/segments`}
+            item="segments"
+          />
         </div>
       ))}
     </div>
