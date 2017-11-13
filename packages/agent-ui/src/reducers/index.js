4ea6f8a749134c0a54eef499f942e7b4ee7f4ea1
Bastien Teinturier <31281497+t-bast@users.noreply.github.com>
2017-11-13 15:37:59 +0100

    agent-ui: store map explorer's selected segment in redux state (#121)

--- packages/agent-ui/src/reducers/index.js
+++ packages/agent-ui/src/reducers/index.js
@@ -4,12 +4,14 @@ import agents from './agents';
 import appendSegment from './appendSegment';
 import createMap from './createMap';
 import maps from './maps';
+import mapExplorer from './mapExplorer';
 import segment from './segment';
 import segments from './segments';
 
 const rootReducer = combineReducers({
   agents,
   maps,
+  mapExplorer,
   segment,
   segments,
   createMap,
