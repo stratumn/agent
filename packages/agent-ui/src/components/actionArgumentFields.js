a87eb6763584a2b4a110cc555c278956a080c2ff
t-bast <bastien@stratumn.com>
2017-11-17 09:11:20 +0100

    agent-ui: use map instead of for loop

--- packages/agent-ui/src/components/actionArgumentFields.js
+++ packages/agent-ui/src/components/actionArgumentFields.js
@@ -3,26 +3,18 @@ import PropTypes from 'prop-types';
 
 import TextField from 'material-ui/TextField';
 
-export const ActionArgumentFields = ({ args, valueChanged }) => {
-  const valueFields = [...Array(args.length)];
-
-  for (let i = 0; i < args.length; i += 1) {
-    const argName = args[i];
-    valueFields[i] = (
-      <TextField
-        key={argName}
-        label={argName}
-        onChange={e => {
-          valueChanged(i, e.target.value);
-        }}
-        type="text"
-        fullWidth
-      />
-    );
-  }
-
-  return valueFields;
-};
+export const ActionArgumentFields = ({ args, valueChanged }) =>
+  args.map((arg, index) => (
+    <TextField
+      key={arg}
+      label={arg}
+      onChange={e => {
+        valueChanged(index, e.target.value);
+      }}
+      type="text"
+      fullWidth
+    />
+  ));
 
 ActionArgumentFields.propTypes = {
   args: PropTypes.arrayOf(PropTypes.string).isRequired,
