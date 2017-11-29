c9144bcfccdb9ab841bc1bd82e21510830c72c73
Conor Dawson <conor.dawson@stratumn.com>
2017-11-29 12:09:53 +0100

    agent-ui: make the linter happy

--- packages/agent-ui/src/containers/refChipList.test.js
+++ packages/agent-ui/src/containers/refChipList.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
