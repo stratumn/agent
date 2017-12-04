9b880c53526208e173e13b83b79b193b0a378f36
Simon Vadée <simon@stratumn.com>
2017-12-04 10:09:13 +0100

    agent-client-js: safely concatenate agent url

--- packages/agent-client-js/test/getAgent.js
+++ packages/agent-client-js/test/getAgent.js
@@ -30,6 +30,17 @@ describe('#getAgent', () => {
         );
       }));
 
+    it('loads an agent if the url has an extra "/" at the end', () => {
+      const url = 'http://localhost:3333/';
+      return getAgent(url).then(agent => {
+        agent.url.should.be.exactly('http://localhost:3333/');
+        Object.keys(agent.processes).length.should.be.exactly(3);
+        agent.processes.first_process.storeInfo.adapter.name.should.be.exactly(
+          'memory'
+        );
+      });
+    });
+
     it('fails to load an agent', () =>
       getAgent()
         .then(() => {
