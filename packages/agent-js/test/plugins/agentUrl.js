1ba7a105a00696abb93b0c32f410fb3a2d68e262
Mourad BEJI <mourad.beji@stratumn.com>
2017-12-11 17:11:15 +0100

    agent-js: use agentUrl filterSegment instead of createSegment

---
+++ packages/agent-js/test/plugins/agentUrl.js
@@ -0,0 +1,35 @@
+/*
+  Copyright 2017 Stratumn SAS. All rights reserved.
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+      http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+import pluginTest from '.';
+import agentUrl from '../../src/plugins/agentUrl';
+
+describe('agentUrl', () => {
+  describe('#filterSegments()', () => {
+    it("should add the agent url to the segments' meta", () => {
+      const segment = {
+        link: {
+          state: 'The link',
+          meta: { process: 'yolo' }
+        },
+        meta: { linkHash: 42 }
+      };
+      return agentUrl('The agent url')
+        .filterSegment(segment)
+        .then(ok => {
+          ok.should.be.ok();
+          segment.meta.agentUrl.should.be.equal('The agent url');
+        });
+    });
+  });
+});
