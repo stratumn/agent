5d31fa384a43082267782ab3aa0312dd81cf8b5f
Adrien Montfort <adrien.montfort@gmail.com>
2017-12-21 15:50:03 +0100

    tmpop-explorer: fix docker image
    - update create-react-app
    - use yarn

--- packages/tmpop-explorer/app/src/index.js
+++ packages/tmpop-explorer/app/src/index.js
@@ -14,27 +14,14 @@
   limitations under the License.
 */
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TMPopExplorer from '@indigoframework/tmpop-explorer';
+
 import './index.css';
 
-let React;
-let ReactDOM;
-let TMPopExplorer;
-
-if (process.env.NODE_ENV === 'development') {
-  // In development, use parent module.
-  React = require('../../node_modules/react');
-  ReactDOM = require('../../node_modules/react-dom');
-  TMPopExplorer = require('../../lib/TMPopExplorer');
-} else {
-  React = require('react');
-  ReactDOM = require('react-dom');
-  TMPopExplorer = require('@indigoframework/tmpop-explorer');
-}
-
-const App = React.createClass({
-  render() {
-    return <TMPopExplorer remote={`${location.hostname}:46657`} />;
-  }
-});
+const App = () => (
+  <TMPopExplorer remote={`${window.location.hostname}:46657`} />
+);
 
 ReactDOM.render(<App />, document.getElementById('app'));
