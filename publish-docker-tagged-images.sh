bc7e7538e360fe096324993673023608d1684c81
Simon Vadée <simon@stratumn.com>
2018-03-15 16:07:54 +0100

    Publish
     - @indigoframework/client@0.3.0-alpha1
     - @indigoframework/agent@0.3.0-alpha1
     - @indigoframework/agent-sample@0.3.0-alpha1
     - @indigoframework/agent-ui@0.3.0-alpha1
     - @indigoframework/angular-mapexplorer@0.3.0-alpha1
     - @indigoframework/angular2-mapexplorer@0.3.0-alpha1
     - @indigoframework/cs-validator@0.3.0-alpha1
     - @indigoframework/mapexplorer-core@0.3.0-alpha1
     - @indigoframework/react-mapexplorer@0.3.0-alpha1
     - @indigoframework/tmpop-explorer@0.3.0-alpha1
     - @indigoframework/utils@0.3.0-alpha1

--- publish-docker-tagged-images.sh
+++ publish-docker-tagged-images.sh
@@ -5,22 +5,22 @@ read VERSION
 
 echo Triggering agent-sample build...
 AGENT_SAMPLE_TOKEN=$(keybase fs read /keybase/team/stratumn_eng/docker_agent_sample_token.dat)
-curl -H "Content-Type: application/json" --data '{"source_type": "Tag", "source_name": "@indigoframework/agent-sample@'"$VERSION"'"}' -X POST https://registry.hub.docker.com/u/indigoframework/agent-sample/trigger/"$AGENT_SAMPLE_TOKEN"/
+curl -H "Content-Type: application/json" --data '{"source_type": "Tag", "source_name": "@indigoframework/agent-sample@'"$VERSION"'"}' -X POST https://registry.hub.docker.com/u/indigocore/agent-sample/trigger/"$AGENT_SAMPLE_TOKEN"/
 printf "\n"
 
 echo Triggering agent-ui build...
 AGENT_UI_TOKEN=$(keybase fs read /keybase/team/stratumn_eng/docker_agent_ui_token.dat)
-curl -H "Content-Type: application/json" --data '{"source_type": "Tag", "source_name": "@indigoframework/agent-ui@'"$VERSION"'"}' -X POST https://registry.hub.docker.com/u/indigoframework/agent-ui/trigger/"$AGENT_UI_TOKEN"/
+curl -H "Content-Type: application/json" --data '{"source_type": "Tag", "source_name": "@indigoframework/agent-ui@'"$VERSION"'"}' -X POST https://registry.hub.docker.com/u/indigocore/agent-ui/trigger/"$AGENT_UI_TOKEN"/
 printf "\n"
 
 echo Triggering agent-js build...
 AGENT_JS_TOKEN=$(keybase fs read /keybase/team/stratumn_eng/docker_agent_js_token.dat)
-curl -H "Content-Type: application/json" --data '{"source_type": "Tag", "source_name": "@indigoframework/agent@'"$VERSION"'"}' -X POST https://registry.hub.docker.com/u/indigoframework/agent-js/trigger/"$AGENT_JS_TOKEN"/
+curl -H "Content-Type: application/json" --data '{"source_type": "Tag", "source_name": "@indigoframework/agent@'"$VERSION"'"}' -X POST https://registry.hub.docker.com/u/indigocore/agent-js/trigger/"$AGENT_JS_TOKEN"/
 printf "\n"
 
 echo Triggering tmpop-explorer build...
 TMPOP_EXPLORER_TOKEN=$(keybase fs read /keybase/team/stratumn_eng/docker_tmpop_explorer_token.dat)
-curl -H "Content-Type: application/json" --data '{"source_type": "Tag", "source_name": "@indigoframework/tmpop-explorer@'"$VERSION"'"}' -X POST https://registry.hub.docker.com/u/indigoframework/tmpop-explorer/trigger/"$TMPOP_EXPLORER_TOKEN"/
+curl -H "Content-Type: application/json" --data '{"source_type": "Tag", "source_name": "@indigoframework/tmpop-explorer@'"$VERSION"'"}' -X POST https://registry.hub.docker.com/u/indigocore/tmpop-explorer/trigger/"$TMPOP_EXPLORER_TOKEN"/
 printf "\n"
 
 echo Docker builds successfully triggered
