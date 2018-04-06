72f097747b1da5ff8a34469d4b45c8e3c7dd2fc3
Alexandre Thibault <pepe@happycoders.org>
2018-04-06 14:19:13 +0200

    *: update copyright (#270)

--- update-license-headers.sh
+++ update-license-headers.sh
@@ -3,18 +3,16 @@
 set -e
 
 update-license-header() {
-    perl -i -0pe 's/\/\*.*\n.*Copyright.*Stratumn.*\n(.*\n)*\*\/\n/`cat LICENSE_HEADER`/ge' $1
+	perl -i -0pe 's/\/\*.*\n.*Copyright.*Stratumn.*\n([\s\S]*?)\*\/(\N*)\n/`cat LICENSE_HEADER`.$2/ge' $1
 }
 
 directories="src test"
 extensions="js less"
 
 for d in $directories; do
-	if [ -d "$d" ]; then
-		for e in $extensions; do
-			for f in $(find packages -regex "packages/[^/]*/$d/.*\.$e"); do
-				update-license-header $f
-			done
+	for e in $extensions; do
+		for f in $(find packages -regex "packages/[^/]*/$d/.*\.$e"); do
+			update-license-header $f
 		done
-	fi
+	done
 done
